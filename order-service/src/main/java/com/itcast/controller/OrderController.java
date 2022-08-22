6051cceb1284c01f9e1b0eed4ce4a26d9304b5c3
“yhyh” <32655@qq.com>
2022-08-22 20:23:09 +0800

    second commit

--- order-service/src/main/java/com/itcast/controller/OrderController.java
+++ order-service/src/main/java/com/itcast/controller/OrderController.java
@@ -2,6 +2,8 @@ package com.itcast.controller;
 
 import com.itcast.entity.Order;
 import com.itcast.service.OrderService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
+@RefreshScope
 @RestController
 @RequestMapping("order")
 public class OrderController {
 
    @Resource
    private OrderService orderService;
+
+    @Value("${service}")
+    private String service ;
+
+    @GetMapping("/get")
+    public String get() {
+        return service;
+    }
 
     @GetMapping("{orderId}")
     public Order queryOrderByUserId(@PathVariable("orderId") Long orderId) {
--- user-service/src/main/java/com/itcast/controller/UserController.java
+++ user-service/src/main/java/com/itcast/controller/UserController.java
@@ -3,27 +3,30 @@ package com.itcast.controller;
 import com.itcast.entity.User;
 import com.itcast.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
 @Slf4j
+@RefreshScope
 @RestController
-@RequestMapping("/user")
+@RequestMapping("user")
 public class UserController {
 
     @Resource
     private UserService userService;
 
-    /**
-     * 路径： /user/110
-     *
-     * @param id 用户id
-     * @return 用户
-     */
+    @Value("${service}")
+    private String service ;
+
+    @GetMapping("/get")
+    public String get() {
+        return service;
+    }
     @GetMapping("/{id}")
-    public User queryById(@PathVariable("id") Long id, @RequestHeader(value = "Truth",required = false) String Truth ,@RequestHeader(value = "truth",required = false) String truth) {
-        System.out.println("Truth==>"+Truth);
+    public User queryById(@PathVariable("id") Long id,@RequestHeader(value = "truth",required = false) String truth) {
         System.out.println("truth==>"+truth);
         return userService.queryById(id);
     }
